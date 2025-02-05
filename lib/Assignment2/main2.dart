a8e940b2568e5ee4c80b176908b2896248ac7251
rubayatkhusbu25 <rubayet.khusbu1@gmail.com>
2025-02-05 22:08:26 +0600

    "commit1"

--- lib/Assignment2/main2.dart
+++ lib/Assignment2/main2.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import '../Live_test2/live_test.dart';
-import 'contact_list.dart';
-
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        designSize: Size(420, 893),
-        builder: (context ,child)=> MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Contact List',
-          theme: ThemeData.light(),
-          home:LiveTest(),
-        )
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import '../Live_test2/live_test.dart';
+// import 'contact_list.dart';
+//
+//
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return ScreenUtilInit(
+//         designSize: Size(420, 893),
+//         builder: (context ,child)=> MaterialApp(
+//           debugShowCheckedModeBanner: false,
+//           title: 'Contact List',
+//           theme: ThemeData.light(),
+//           home:LiveTest(),
+//         )
+//     );
+//   }
+// }
