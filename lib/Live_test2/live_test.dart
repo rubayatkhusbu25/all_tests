a005bf0c89e7d149e4824e194bbd25a299478eda
rubayatkhusbu25 <rubayet.khusbu1@gmail.com>
2025-02-05 23:24:12 +0600

    "Live Test of UI"

--- lib/Live_test2/live_test.dart
+++ lib/Live_test2/live_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LiveTest extends StatefulWidget {
   const LiveTest({super.key});
@@ -8,11 +9,102 @@ class LiveTest extends StatefulWidget {
 }
 
 class _LiveTestState extends State<LiveTest> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      
+      appBar: AppBar(
+        title: Text(
+          "Add Employe",
+          style: TextStyle(
+              fontSize: 20.sp,
+              color: Colors.black,
+              fontWeight: FontWeight.w400),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15.w),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter a name";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  hintText: "Name",
+                  hintStyle: TextStyle(fontSize: 18.sp, color: Colors.grey),
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey)),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)),
+                ),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter age";
+                  } else if (int.tryParse(value) == null) {
+                    return "Enter a valid number";
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: "Age",
+                  hintStyle: TextStyle(fontSize: 18.sp, color: Colors.grey),
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey)),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)),
+                ),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter salary";
+                  } else if (double.tryParse(value) == null) {
+                    return "Enter a valid number";
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: "Salary",
+                  hintStyle: TextStyle(fontSize: 18.sp, color: Colors.grey),
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey)),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)),
+                ),
+              ),
+              SizedBox(
+                height: 15.h,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("Employee Added Successfully!")),
+                      );
+                    }
+                  },
+                  child: Text("Add Employee"))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
